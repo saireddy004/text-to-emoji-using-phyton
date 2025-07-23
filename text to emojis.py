755dbb805a3193ca03ee0f17d4aec6a5b8867d9a
Kothakapu bhoomreddy <155342638+saireddy004@users.noreply.github.com>
2025-07-23 10:32:40 +0530

    Add files via upload

---
+++ text to emojis.py
@@ -0,0 +1,11 @@
+
+import demoji
+
+demoji.download_codes()
+
+text ="""I will be your secret santa this year 🎅.
+        The party im hosting for you is going to be lit🔥💩.
+        A happiness of 🧑‍🎄🧑‍🎄 will erupt that day.
+        Have fun till then you 🤡🧁🧔🏽🥂."""
+
+demoji.findall(text)
